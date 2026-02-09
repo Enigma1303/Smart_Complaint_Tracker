3ab1417ae04777cb68df153a9d7061309101472f
Aryan Kumar <kumar.aryan.1303@gmail.com>
2026-02-09 20:14:49 +0530

    Fixed drop downs not showing in filters and added ordering to sort by urgency score

--- app/complaints/views.py
+++ app/complaints/views.py
@@ -1,36 +1,63 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import (extend_schema,
+                                OpenApiParameter, 
+                                extend_schema_view,
+                                OpenApiTypes)
+
+from rest_framework.filters import OrderingFilter
 
 from .models import Complaint
 from .serializers import ComplaintSerializer,ComplaintUpdateSerializer
 
-@extend_schema(
+@extend_schema_view(
+list=extend_schema(
     parameters=[
         OpenApiParameter(
             name="status",
             description="Filter by complaint status (OPEN, IN_PROGRESS, RESOLVED)",
             required=False,
-            type=str,
+            type=OpenApiTypes.STR,
+            enum=[choice[0] for choice in Complaint.Status.choices]
         ),
         OpenApiParameter(
             name="category",
             description="Filter by complaint category (ROAD, WATER, ELECTRICITY)",
             required=False,
-            type=str,
+            type=OpenApiTypes.STR,
+            enum=[choice[0] for choice in Complaint.Category.choices]
         ),
         OpenApiParameter(
             name="userId",
             description="Set ID  to get complaints created by the logged-in user",
             required=False,
-            type=int,
+            type=OpenApiTypes.INT,
+        ),
+        OpenApiParameter(
+            name="ordering",
+            description="Order by 'created_at' or 'urgency_score'. Prefix with '-' for descending.",
+            required=False,
+            type=OpenApiTypes.STR,
+            enum=[
+                   "created_at",
+                   "-created_at",
+                   "urgency_score",
+                "-urgency_score",
+    ],
         ),
     ]
+)
 )
 
 class ComplaintViewSet(viewsets.ModelViewSet):
 
     permission_classes=[IsAuthenticated]
+
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["created_at", "urgency_score"]
+    ordering = ["-created_at"]
+
     def get_queryset(self):
         queryset=Complaint.objects.all()
         status_param=self.request.query_params.get("status")
