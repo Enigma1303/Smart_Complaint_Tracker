be23788ff54a95583f8d3646b31838c960eea324
Aryan Kumar <kumar.aryan.1303@gmail.com>
2026-02-10 11:33:07 +0530

    Added tests for signup and without email signup attempt

--- app/useraccounts/tests.py
+++ app/useraccounts/tests.py
@@ -1,6 +1,32 @@
 from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
 from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
 
+
+class SignupAPITest(APITestCase):
+
+    def test_signup_creates_user(self):
+        url = reverse("signup")
+
+        data = {
+            "email": "signup@example.com",
+            "password": "password123",
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(email="signup@example.com")
+        self.assertTrue(user.check_password("password123"))
+
+    def test_signup_without_email_fails(self):
+        url = reverse("signup")
+        response = self.client.post(url, {"password": "123"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+ 
 
 class UserModelTest(TestCase):
     def test_create_user_with_email(self):
--- app/useraccounts/views.py
+++ app/useraccounts/views.py
@@ -1,16 +1,28 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
 from .serializers import UserSignupSerializer
-
+import logging
 from rest_framework import generics,permissions
 
-
+logger = logging.getLogger(__name__)
 
 class SignupView(generics.CreateAPIView):
     """Signup view with overiding permission classes"""
 
     serializer_class=UserSignupSerializer
     permission_classes=[permissions.AllowAny]
+
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        logger.info(
+            "User signed up",
+            extra={
+                "event": "USER_SIGNUP",
+                "user_id": user.id,
+                "email": user.email,
+            },
+        )
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
